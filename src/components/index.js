1617285f31bbd5946d9c5ae4ad5df88c9d90aa19
徐烽烽 <xufengfeng@hyit.com.cn>
2019-08-14 23:22:27 +0800

    init

--- src/components/index.js
+++ src/components/index.js
@@ -3,8 +3,6 @@ import Vue from 'vue'
 let components = require.context('./', true, /index.vue$/)
 
 components.keys().forEach(c => {
-  let csp = c.split('/')
-  let name = csp[1]
   let comp = components(c).default || components(c)
-  Vue.component(name, comp)
+  Vue.component(comp.name, comp)
 })
