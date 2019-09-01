b25a5b99fe349cce5c075822eb30a49760d4602f
徐烽烽 <xufengfeng@hyit.com.cn>
2019-09-01 11:24:56 +0800

    init

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 
-let components = require.context('./', true, /index.vue$/)
+let requireComponent = require.context('./', true, /index.vue$/)
 
-components.keys().forEach(c => {
-  let comp = components(c).default || components(c)
-  Vue.component(comp.name, comp)
+let reg = /\.\/(\S+)\/index\.vue$/
+
+requireComponent.keys().forEach(c => {
+  let componentName = c.match(reg)[1]
+  let component = requireComponent(c).default || requireComponent(c)
+  Vue.component(componentName, component)
 })
