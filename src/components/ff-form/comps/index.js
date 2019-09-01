0af3c6094e82bc268837381155fc9c7fd47b34ea
徐烽烽 <xufengfeng@hyit.com.cn>
2019-09-01 16:00:39 +0800

    init

--- src/components/ff-form/comps/index.js
+++ src/components/ff-form/comps/index.js
@@ -1,14 +1,16 @@
+import camelCase from 'lodash/camelCase'
+import upperFirst from 'lodash/upperFirst'
+
 const requireComponent = require.context('./', true, /.vue$/)
-const camelCase = require('lodash/camelCase')
-const upperFirst = require('lodash/upperFirst')
+
+const reg = /\.\/(\S+)\.vue$/
 
 let components = {}
-let reg = /\.\/(\S+)\.vue$/
 
 requireComponent.keys().forEach(c => {
   let fileName = reg.exec(c)[1]
   let componentName = upperFirst(camelCase(fileName))
   components[componentName] = requireComponent(c).default || requireComponent(c)
 })
-console.log(components)
+
 export default components
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
+import camelCase from 'lodash/camelCase'
+import upperFirst from 'lodash/upperFirst'
 
-let requireComponent = require.context('./', true, /index.vue$/)
+const requireComponent = require.context('./', true, /index.vue$/)
 
-let reg = /\.\/(\S+)\/index\.vue$/
+const reg = /\.\/(\S+)\/index\.vue$/
 
 requireComponent.keys().forEach(c => {
-  let componentName = c.match(reg)[1]
+  let fileName = c.match(reg)[1]
+  let componentName = upperFirst(camelCase(fileName))
   let component = requireComponent(c).default || requireComponent(c)
   Vue.component(componentName, component)
 })
