b2d2febfccc77607657b13c3ea6d86c314a69b69
徐烽烽 <xufengfeng@hyit.com.cn>
2019-09-22 16:43:05 +0800

    init

---
+++ src/views/loginout/loginout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="loginout"></div>
+</template>
+
+<script>
+export default {
+  name: 'loginout',
+  data () {
+    return {}
+  },
+  beforeCreate () {
+    this.$router.push('/')
+    sessionStorage.removeItem('ff_store')
+    setTimeout(_ => {
+      window.location.reload()
+    }, 200)
+  }
+}
+</script>
+
+<style scoped>
+</style>
