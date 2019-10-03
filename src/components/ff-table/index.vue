2841a3194ff59d20c75018cbf5711a0cffda0b72
徐烽烽 <xufengfeng@hyit.com.cn>
2019-10-03 21:44:54 +0800

    table drop

--- src/components/ff-table/index.vue
+++ src/components/ff-table/index.vue
@@ -24,21 +24,21 @@
       align="center"
       width="50">
     </el-table-column>
-    <template v-for="(item, idx) in titles">
+    <template v-for="item in titles">
       <el-table-column
         v-if="item.render"
-        :key="idx"
+        :key="item.prop"
         :align="item.align || 'center'"
         :label="item.label"
         :min-width="item.width"
       >
         <template v-slot="scope">
-          <render v-if="item.render" :key="idx" :render="item.render" :row="scope.row" :column="item" :index="getIndex(scope.row)"></render>
+          <render v-if="item.render" :key="item.prop" :render="item.render" :row="scope.row" :column="item" :index="getIndex(scope.row)"></render>
         </template>
       </el-table-column>
       <el-table-column
         v-else-if="item.slot"
-        :key="idx"
+        :key="item.prop"
         :align="item.align || 'center'"
         :label="item.label"
         :min-width="item.width"
@@ -49,12 +49,13 @@
       </el-table-column>
       <template v-else>
         <el-table-column
-          :key="idx"
+          :key="item.prop"
           :align="item.align || 'center'"
           :prop="item.prop"
           :label="item.label"
           :min-width="item.width"
           :formatter="item.formatter"
+          :show-overflow-tooltip="item.showTooltip || false"
         >
           <template v-slot="scope" v-if="!item.formatter">
             <template v-if="checkVal(scope.row[item.prop])">{{placeholder}}</template>
@@ -92,7 +93,9 @@
  * @param {boolean} isSelection 是否多选
  * @param {boolean} isRadio 是否单选
  * @param {boolean} isBorder 是否带边框
- * @param {string} rowKey 行数据的 Key
+ * @param {boolean} dropColumn 表格列是否可拖拽
+ * @param {boolean} dropRow 表格行是否可拖拽， 需要设置rowKey
+ * @param {string} rowKey 行数据的 Key（唯一标识）
  * @param {object} treeProps 渲染嵌套数据的配置选项
  * @param {boolean} isLazy 是否懒加载子节点数据
  * @param {function} load 加载子节点数据的函数，lazy 为 true 时生效，函数第二个参数包含了节点的层级信息 Function(row, treeNode, resolve)
@@ -126,11 +129,13 @@
  * 表头数据
  * titles
  * @param {string} label 显示的标题
- * @param {string} prop 对应列内容的字段名，也可以使用 property 属性
+ * @param {string} prop 对应列内容的字段名，也可以使用 property 属性, 如果使用拖拽则不能为空
  * @param {string} width 对应列的最小宽度，默认50px, 与 width 的区别是 width 是固定的，min-width 会把剩余宽度按比例分配给设置了 min-width 的列
  * @param {string} slot 自定义列的插槽名, 参数为{ row }
  * @param {string} align： 对齐方式  left/center/right  默认center
+ * @param {boolean} showTooltip 当内容过长被隐藏时显示 tooltip
  * @param {function} render 自定义渲染列(使用render会覆盖slot)， 使用 Vue 的 Render 函数。接受4个参数 (h, row, column, index)。row、column、index，分别指当前行数据，当前列数据，当前是第几行
+ * @param {function} formatter 用来格式化内容 Function(row, column, cellValue, index)
  * */
 
 /**
@@ -177,6 +182,14 @@ export default {
       default: false
     },
     isLazy: {
+      type: Boolean,
+      default: false
+    },
+    dropColumn: {
+      type: Boolean,
+      default: false
+    },
+    dropRow: {
       type: Boolean,
       default: false
     },
@@ -266,18 +279,33 @@ export default {
       })
     },
     columnDrop () {
-      const wrapperTr = document.querySelector('.el-table__header-wrapper tr')
-      this.sortable = Sortable.create(wrapperTr, {
-        animation: 180,
-        delay: 0,
-        onEnd: evt => {
-          [this.titles[evt.oldIndex], this.titles[evt.newIndex]] = [this.titles[evt.newIndex], this.titles[evt.oldIndex]]
-        }
-      })
+      if (this.dropColumn) {
+        const wrapperTr = document.querySelector('.el-table__header-wrapper tr')
+        this.sortable = Sortable.create(wrapperTr, {
+          animation: 180,
+          delay: 0,
+          onEnd: e => {
+            let oldTit = this.titles.splice(e.oldIndex, 1)
+            this.titles.splice(e.newIndex, 0, ...oldTit)
+          }
+        })
+      }
+    },
+    rowDrop () {
+      if (this.dropRow) {
+        const tbody = document.querySelector('.el-table__body-wrapper tbody')
+        Sortable.create(tbody, {
+          onEnd: e => {
+            let oldTit = this.data.splice(e.oldIndex, 1)
+            this.data.splice(e.newIndex, 0, ...oldTit)
+          }
+        })
+      }
     }
   },
   mounted () {
     this.columnDrop()
+    this.rowDrop()
   }
 }
 </script>
--- src/views/demo/menu1-2/menu1-2.vue
+++ src/views/demo/menu1-2/menu1-2.vue
@@ -4,6 +4,9 @@
       style="width: 80%; margin: 0 auto;"
       :titles="titles"
       :data="tableData"
+      rowKey="id"
+      dropColumn
+      dropRow
     ></ff-table>
   </div>
 </template>
@@ -18,27 +21,31 @@ export default {
         { label: 'name', prop: 'name' },
         { label: 'sort', prop: 'sort' },
         { label: 'address', prop: 'address' },
-        { label: 'status', formatter: row => row.status === '1' ? '是' : '否' }
+        { label: 'status', prop: 'status', formatter: row => row.status === '1' ? '是' : '否' }
       ],
       tableData: [{
+        id: 1,
         date: '2016-05-02',
         name: 'b',
         address: '上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄上海市普陀区金沙江路 1518 弄',
         sort: '4',
         status: '0'
       }, {
+        id: 2,
         date: '2016-05-04',
         name: 'c',
         address: '上海市普陀区金沙江路 1517 弄',
         sort: '2',
         status: '1'
       }, {
+        id: 3,
         date: '2016-05-01',
         name: 'd',
         address: '上海市普陀区金沙江路 1519 弄',
         sort: '1',
         status: '1'
       }, {
+        id: 4,
         date: '2016-05-03',
         name: 'a',
         address: '上海市普陀区金沙江路 1516 弄',
