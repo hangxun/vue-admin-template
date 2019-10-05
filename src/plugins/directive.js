c9ac697c50e4a454abc2135c5fed638d8ca0b80c
徐烽烽 <xufengfeng@hyit.com.cn>
2019-10-05 16:56:52 +0800

    init

--- src/plugins/directive.js
+++ src/plugins/directive.js
@@ -1,14 +1,42 @@
+const directives = {
+  /**
+   * 图片加载失败显示404图片
+   * v-img-err="图片url（可省略，默认@/assets/404.gif）"
+   * */
+  imgErr (el, binding, vnode) {
+    let errimg = binding.value || require('@/assets/404.gif')
+    el.addEventListener('error', e => {
+      e.target.src = errimg
+    })
+  },
+  /**
+   * 点击el及其子元素以外区域时执行，若使用self修饰符则不包括子元素
+   * v-click-outside.self="方法名"
+   * */
+  clickOutside: {
+    clickOutsideValue: {},
+    bind: (el, binding, vnode) => {
+      directives.clickOutside.clickOutsideValue[binding.expression] = (e) => {
+        if (binding.modifiers && binding.modifiers.self) {
+          if (e.target !== el && binding.value) binding.value()
+        } else {
+          if (!el.contains(e.target) && binding.value) binding.value()
+        }
+      }
+      document.addEventListener('click', directives.clickOutside.clickOutsideValue[binding.expression])
+    },
+    unbind: (el, binding, vnode) => {
+      document.removeEventListener('click', directives.clickOutside.clickOutsideValue[binding.expression])
+      Reflect.deleteProperty(directives.clickOutside.clickOutsideValue, [binding.expression])
+      console.log(directives.clickOutside.clickOutsideValue)
+    }
+  }
+}
+
 export default {
   install (Vue) {
     Vue.mixin({
-      directives: {
-        imgErr (el, binding, vnode) { // 图片加载失败显示404图片
-          let errimg = binding.value || require('@/assets/404.gif')
-          el.addEventListener('error', e => {
-            e.target.src = errimg
-          })
-        }
-      }
+      directives
     })
   }
 }
--- src/views/demo/menu4/menu4.vue
+++ src/views/demo/menu4/menu4.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="menu4">
     menu4
+    <div class="box" ref="box" v-click-outside="con"></div>
+    <div class="ch" v-click-outside="con2"></div>
     <router-view />
   </div>
 </template>
@@ -10,9 +12,30 @@ export default {
   name: 'menu4',
   data () {
     return {}
+  },
+  methods: {
+    con () {
+      console.log(111)
+    },
+    con2 () {
+      console.log(222)
+    },
+    contains (e) {
+      console.log(this.$refs.box.contains(e.target))
+    }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .box {
+    width: 200px;
+    height: 200px;
+    background-color: #009900;
+  }
+  .ch {
+    width: 100px;
+    height: 100px;
+    background-color: red;
+  }
 </style>
