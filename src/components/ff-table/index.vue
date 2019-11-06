d23d647b3a12a8890250f8682c5ca264e11cd7f3
徐烽烽 <xufengfeng@hyit.com.cn>
2019-11-06 15:09:06 +0800

    init

--- src/components/ff-table/index.vue
+++ src/components/ff-table/index.vue
@@ -1,88 +1,143 @@
 <template>
-  <el-table
-    v-set-id
-    style="width: 100%"
-    class="ff-table"
-    ref="table"
-    :data="data"
-    :border="isBorder"
-    :highlight-current-row="isRadio"
-    :row-key="rowKey"
-    :tree-props="treeProps"
-    :lazy="isLazy"
-    :load="load"
-    @row-click="rowClick"
-    @selection-change="selectionChange"
-    @current-change="handleCurrentChange">
-    <el-table-column
-      v-if="isSelection"
-      type="selection"
-      width="55">
-    </el-table-column>
-    <el-table-column
-      v-if="isShowIndex"
-      type="index"
-      align="center"
-      width="50">
-    </el-table-column>
-    <template v-for="item in titles">
+  <div class="ff-table">
+    <div class="export">
+      <el-dropdown v-if="exportFile">
+        <el-button type="primary">
+          导出<i class="el-icon-arrow-down el-icon--right"></i>
+        </el-button>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item @click.native="exportExcel">excel</el-dropdown-item>
+          <el-dropdown-item @click.native="exportCsv">csv</el-dropdown-item>
+          <el-dropdown-item @click.native="exportPdf">pdf</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+    <el-table
+      v-set-id
+      style="width: 100%"
+      class="ff-el-table"
+      ref="table"
+      :data="data"
+      :border="isBorder"
+      :highlight-current-row="isRadio"
+      :row-key="rowKey"
+      :tree-props="treeProps"
+      :lazy="lazy"
+      :load="load"
+      @row-click="_rowClick"
+      @row-dblclick="_rowDblclick"
+      @selection-change="_selectionChange"
+      @current-change="_handleCurrentChange"
+      @sort-change="_sortChange"
+    >
       <el-table-column
-        v-if="item.render"
-        class-name="allow-drop-column"
-        :key="getDynamicProp(item.prop)"
-        :align="item.align || 'center'"
-        :label="item.label"
-        :min-width="item.width"
-      >
-        <template v-slot="scope">
-          <render v-if="item.render" :key="getDynamicProp(item.prop)" :render="item.render" :row="scope.row" :column="item" :index="getIndex(scope.row)"></render>
-        </template>
+        v-if="isSelection"
+        align="center"
+        type="selection"
+        width="50">
       </el-table-column>
       <el-table-column
-        v-else-if="item.slot"
-        class-name="allow-drop-column"
-        :key="getDynamicProp(item.prop)"
-        :align="item.align || 'center'"
-        :label="item.label"
-        :min-width="item.width"
-      >
-        <template v-slot="scope">
-          <slot :name="item.slot" :row="scope.row"></slot>
-        </template>
+        v-if="isShowIndex"
+        type="index"
+        align="center"
+        width="50">
       </el-table-column>
-      <template v-else>
+      <template v-for="item in titles">
         <el-table-column
+          v-if="item.render && !item.hidden"
           class-name="allow-drop-column"
-          :align="item.align || 'center'"
-          :key="getDynamicProp(item.prop)"
+          :key="_getDynamicProp(item.prop)"
+          :align="item.align || align"
+          :label="item.label"
+          :min-width="item.width"
+          :sortable="item.sortable || false"
+          :sort-by="item.sortBy"
+          :sort-method="item.sortMethod"
+        >
+          <template #header="scope" v-if="_hasHeader(item.prop)">
+            <slot :name="'header-' + item.prop" :column="scope.column"></slot>
+          </template>
+          <template v-slot="scope">
+            <render v-if="item.render" :key="_getDynamicProp(item.prop)" :render="item.render" :row="scope.row" :column="item" :index="_getIndex(scope.row)"></render>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-else-if="item.slot && !item.hidden"
+          class-name="allow-drop-column"
+          :key="_getDynamicProp(item.prop)"
+          :align="item.align || align"
+          :label="item.label"
+          :min-width="item.width"
+          :sortable="item.sortable || false"
+          :sort-by="item.sortBy"
+          :sort-method="item.sortMethod"
+        >
+          <template #header="scope" v-if="_hasHeader(item.prop)">
+            <slot :name="'header-' + item.prop" :column="scope.column"></slot>
+          </template>
+          <template v-slot="scope">
+            <slot :name="item.slot" :row="scope.row"></slot>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-else-if="!item.hidden"
+          class-name="allow-drop-column"
+          :align="item.align || align"
+          :key="_getDynamicProp(item.prop)"
           :prop="item.prop"
           :label="item.label"
           :min-width="item.width"
+          :sortable="item.sortable || false"
+          :sort-by="item.sortBy"
+          :sort-method="item.sortMethod"
           :formatter="item.formatter"
           :show-overflow-tooltip="item.showTooltip || false"
         >
+          <template #header="scope" v-if="_hasHeader(item.prop)">
+            <slot :name="'header-' + item.prop" :column="scope.column"></slot>
+          </template>
           <template v-slot="scope" v-if="!item.formatter">
-            <template v-if="checkVal(scope.row[item.prop])">{{placeholder}}</template>
+            <template v-if="_checkVal(scope.row[item.prop])">{{placeholder}}</template>
             <template v-else>{{scope.row[item.prop]}}</template>
           </template>
         </el-table-column>
       </template>
-    </template>
-    <el-table-column
-      v-if="isAdd || isEdit || isLook || isDel"
-      align="center"
-      label="操作"
-      min-width="50">
-      <template v-slot="scope">
-        <slot name="btnPrepend" :row="scope.row"></slot>
-        <el-button type="text" v-if="isAdd" icon="el-icon-plus" @click.stop="handleAdd(scope.row)"></el-button>
-        <el-button class="centerBtn" type="text" v-if="isEdit" icon="el-icon-edit-outline" @click.stop="handleEdit(scope.row)"></el-button>
-        <el-button type="text" v-if="isLook" icon="el-icon-document" @click.stop="handleLook(scope.row)"></el-button>
-        <el-button type="text" v-if="isDel" icon="el-icon-delete" @click.stop="handleDel(scope.row)"></el-button>
-        <slot name="btnAppend" :row="scope.row"></slot>
-      </template>
-    </el-table-column>
-  </el-table>
+      <el-table-column
+        v-if="handlerShow"
+        align="center"
+        label="操作"
+        min-width="50">
+        <template v-slot="scope">
+          <slot name="btnPrepend" :row="scope.row"></slot>
+          <el-button type="text" v-if="isAdd" icon="el-icon-plus" @click.stop="_handleAdd(scope.row)"></el-button>
+          <el-button class="centerBtn" type="text" v-if="isEdit" icon="el-icon-edit-outline" @click.stop="_handleEdit(scope.row)"></el-button>
+          <el-button type="text" v-if="isLook" icon="el-icon-document" @click.stop="_handleLook(scope.row)"></el-button>
+          <el-button type="text" v-if="isDel" icon="el-icon-delete" @click.stop="_handleDel(scope.row)"></el-button>
+          <slot name="btnAppend" :row="scope.row"></slot>
+        </template>
+      </el-table-column>
+      <el-table-column v-if="columControl" width="50">
+        <template #header="{ row }">
+          <el-button icon="el-icon-menu" size="mini" circle @click="controlShow = !controlShow"></el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog
+      title="字段显隐控制"
+      :visible.sync="controlShow"
+      class="dialogCustom"
+      width="20%"
+      :before-close="_beforeCloseControl"
+    >
+      <el-checkbox-group v-model="selection">
+        <el-checkbox v-for="(check, index) in titles" :key="index" :label="check.prop" style="display: block;margin: 10px;">{{check.label}}</el-checkbox>
+      </el-checkbox-group>
+      <span slot="footer" class="dialog-footer">
+        <el-button size="medium" @click="_cancelControl">取 消</el-button>
+        <el-button size="medium" type="primary" @click="_confirmControl">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
@@ -93,25 +148,27 @@
    *
    * 可选参数
    * @param {Array|string} handler 操作栏显示的按钮类型，数组或“，”隔开的字符串，['add', 'edit', 'look', 'del']
-   * @param {boolean} isShowIndex 是否显示索引
-   * @param {boolean} isSelection 是否多选
-   * @param {boolean} isRadio 是否单选
-   * @param {boolean} isBorder 是否带边框
-   * @param {boolean} dropColumn 表格列是否可拖拽
-   * @param {boolean} dropRow 表格行是否可拖拽， 需要设置rowKey
-   * @param {string} rowKey 行数据的 Key（唯一标识）
-   * @param {object} treeProps 渲染嵌套数据的配置选项
-   * @param {boolean} isLazy 是否懒加载子节点数据
+   * @param {boolean} is-show-index 是否显示索引
+   * @param {boolean} is-selection 是否多选
+   * @param {boolean} is-radio 是否单选
+   * @param {boolean} is-border 是否带边框
+   * @param {boolean} drop-column 表格列是否可拖拽
+   * @param {boolean} drop-row 表格行是否可拖拽， 需要设置rowKey
+   * @param {string} row-key 行数据的 Key（唯一标识）
+   * @param {object} tree-props 渲染嵌套数据的配置选项, 默认{ hasChildren: 'hasChildren', children: 'children' }
+   * @param {boolean} lazy 是否懒加载子节点数据
    * @param {function} load 加载子节点数据的函数，lazy 为 true 时生效，函数第二个参数包含了节点的层级信息 Function(row, treeNode, resolve)
    * @param {string} placeholder 当前项数据为''、null、undefined时显示的文本
+   * @param {string} align 对齐方式
+   * @param {boolean} export-file 是否显示导出按钮
    * *
-   * @function selectionChange 当选择项发生变化时会触发该事件
+   * @function selection-change 当选择项发生变化时会触发该事件
    * @param {Array} selection 已选中的数组
    * *
-   * @function currentChange 当表格的当前行发生变化的时候会触发该事件，如果要高亮当前行，请打开表格的 isRadio 属性
+   * @function current-change 当表格的当前行发生变化的时候会触发该事件，如果要高亮当前行，请打开表格的 isRadio 属性
    * @param {Object} row 当前行数据
    * *
-   * @function rowClick 当某一行被点击时会触发该事件
+   * @function row-click 当某一行被点击时会触发该事件
    * @param {Object} row
    * @param {Object} column
    * @param {Object} event
@@ -140,6 +197,12 @@
    * @param {boolean} showTooltip 当内容过长被隐藏时显示 tooltip
    * @param {function} render 自定义渲染列(使用render会覆盖slot)， 使用 Vue 的 Render 函数。接受4个参数 (h, row, column, index)。row、column、index，分别指当前行数据，当前列数据，当前是第几行
    * @param {function} formatter 用来格式化内容 Function(row, column, cellValue, index)
+   * @param {boolean|string} sortable 是否排序，可选值true, false, 'custom'
+   * @param {string|Array|function} sortBy 指定数据按照哪个属性进行排序，仅当 sortable 设置为 true 且没有设置 sort-method 的时候有效。如果 sort-by 为数组，则先按照第 1 个属性排序，如果第 1 个相等，再按照第 2 个排序，以此类推
+   * @param {function} sortMethod 对数据进行排序的时候使用的方法，仅当 sortable 设置为 true 的时候有效，需返回一个数字，和 Array.sort 表现一致
+   *
+   * 自定义表头
+   * 在标签中直接插入插槽名为header-[prop], 参数为{ column }
    * */
   
   /**
@@ -151,13 +214,18 @@
    * @param del 删除按钮
    *
    * 操作栏扩展插槽
-   * btnPrepend 操作栏前置内容
-   * btnAppend 操作栏后置内容
+   * btnPrepend 操作栏前置内容, 参数为{ row }
+   * btnAppend 操作栏后置内容, 参数为{ row }
    * */
   import Sortable from 'sortablejs'
+  import FileSaver from 'file-saver'
+  import XLSX from 'xlsx'
+  import html2Canvas from 'html2canvas'
+  import JsPDF from 'jspdf'
   import trim from 'lodash/trim'
+  import cloneDeep from 'lodash/cloneDeep'
   export default {
-    name: 'FfTable',
+    name: 'HyFfTable',
     components: {
       render: _ => import('./render')
     },
@@ -197,7 +265,7 @@
         type: Boolean,
         default: false
       },
-      isLazy: {
+      lazy: {
         type: Boolean,
         default: false
       },
@@ -206,6 +274,14 @@
         default: true
       },
       dropRow: {
+        type: Boolean,
+        default: false
+      },
+      columControl: {
+        type: Boolean,
+        default: false
+      },
+      exportFile: {
         type: Boolean,
         default: false
       },
@@ -222,11 +298,18 @@
         type: Object,
         default: _ => ({ hasChildren: 'hasChildren', children: 'children' })
       },
-      load: Function
+      load: Function,
+      align: {
+        type: String,
+        default: 'center'
+      }
     },
     data () {
       return {
-        dropColumnCount: 0
+        dropColumnCount: 0,
+        controlShow: false,
+        selection: [],
+        oldSelection: []
       }
     },
     computed: {
@@ -246,44 +329,71 @@
       },
       isDel () {
         return this.handleList.includes('del')
+      },
+      isHasHandlerSlot () {
+        let slots = Object.keys(this.$scopedSlots)
+        return slots.some(v => v === 'btnPrepend' || v === 'btnAppend')
+      },
+      handlerShow () {
+        const handlerFlags = [this.isHasHandlerSlot, this.isAdd, this.isEdit, this.isLook, this.isDel]
+        return handlerFlags.some(v => v)
+      }
+    },
+    watch: {
+      titles: {
+        handler () {
+          this._syncTitles()
+        },
+        immediate: true,
+        deep: true
       }
     },
     methods: {
-      getDynamicProp (prop) {
+      _getDynamicProp (prop) {
         return `${prop}${this.dropColumnCount}`
       },
-      checkVal (val) {
+      _hasHeader (prop) {
+        let slots = Object.keys(this.$scopedSlots)
+        return slots.includes(`header-${prop}`)
+      },
+      _checkVal (val) {
         if (val === null || val === undefined || val === '') return true
         else return false
       },
-      getIndex (row) {
+      _getIndex (row) {
         return this.data.findIndex(v => v === row)
       },
-      rowClick (row, column, event) {
+      _rowClick (row, column, event) {
         this.$emit('row-click', row, column, event)
         if (this.isSelection) {
           this.toggleRowSelection(row)
         }
       },
-      selectionChange (selection) {
-        this.$emit('selectionChange', selection)
+      _rowDblclick (row, column, event) {
+        this.$emit('row-dblclick', row, column, event)
       },
-      handleCurrentChange (row) {
-        this.$emit('currentChange', row)
+      _selectionChange (selection) {
+        this.$emit('selection-change', selection)
+      },
+      _handleCurrentChange (row) {
+        this.$emit('current-change', row)
+      },
+      _sortChange ({ column, prop, order }) {
+        this.$emit('sort-change', { column, prop, order })
       },
       toggleRowSelection (row) {
         this.$refs.table.toggleRowSelection(row)
       },
-      handleAdd (row) {
+      _handleAdd (row) {
         this.$emit('add', row)
       },
-      handleEdit (row) {
+      _handleEdit (row) {
         this.$emit('edit', row)
       },
-      handleLook (row) {
+      _handleLook (row) {
         this.$emit('look', row)
       },
-      handleDel (row) {
+      _handleDel (row) {
         this.$confirm('是否确认删除?', '提示', {
           confirmButtonText: '确定',
           cancelButtonText: '取消',
@@ -293,11 +403,11 @@
         }).catch(() => {
         })
       },
-      columnDrop () {
+      _columnDrop () {
         if (this.dropColumn) {
           let oldSort
           const oTable = this.$refs.table.$el
-          const wrapperTr = oTable.querySelector('.ff-table .el-table__header-wrapper tr')
+          const wrapperTr = oTable.querySelector('.el-table__header-wrapper tr')
           let sortable = Sortable.create(wrapperTr, {
             animation: 180,
             delay: 0,
@@ -314,7 +424,7 @@
               const maxColumn = this.titles.length + subLength
               if (newIndex < subLength) {
                 sortable.sort(oldSort)
-              } else if (this.handleList.length && newIndex === maxColumn) {
+              } else if (this.handleList.length && newIndex >= maxColumn) {
                 sortable.sort(oldSort)
               } else {
                 oldIndex -= subLength
@@ -327,10 +437,10 @@
           })
         }
       },
-      rowDrop () {
+      _rowDrop () {
         if (this.dropRow) {
           const oTable = this.$refs.table.$el
-          const tbody = oTable.querySelector('.ff-table .el-table__body-wrapper tbody')
+          const tbody = oTable.querySelector('.el-table__body-wrapper tbody')
           Sortable.create(tbody, {
             animation: 180,
             delay: 0,
@@ -340,11 +450,99 @@
             }
           })
         }
+      },
+      _syncTitles () {
+        let selection = this.titles.filter(v => !v.hidden).map(c => c.prop)
+        this.selection = selection
+        this.oldSelection = selection
+      },
+      _setHiddenTitle () {
+        this.titles.forEach(c => {
+          if (!this.selection.includes(c.prop)) {
+            this.$set(c, 'hidden', true)
+          } else {
+            Reflect.deleteProperty(c, 'hidden')
+          }
+        })
+      },
+      _confirmControl () {
+        this._setHiddenTitle()
+        this.controlShow = false
+        this.oldSelection = cloneDeep(this.selection)
+      },
+      _cancelControl () {
+        this.controlShow = false
+        this.selection = cloneDeep(this.oldSelection)
+      },
+      _beforeCloseControl (done) {
+        this.selection = cloneDeep(this.oldSelection)
+        done()
+      },
+      openMsgBox (cb) {
+        this.$prompt('请输入导出文件名', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          inputValue: `导出文件_${Date.now()}`,
+        }).then(({ value }) => {
+          cb && cb(value)
+        })
+      },
+      exportExcel () {
+        this.openMsgBox(dirname => {
+          this._exportFile(dirname, 'xlsx')
+        })
+      },
+      exportCsv () {
+        this.openMsgBox(dirname => {
+          this._exportFile(dirname, 'csv')
+        })
+      },
+      _exportFile (dirname, suffix) {
+        var wb = XLSX.utils.table_to_book(this.$refs.table.$el)
+        var wbout = XLSX.write(wb, { bookType: suffix, bookSST: true, type: 'array' })
+        try {
+          FileSaver.saveAs(new Blob([wbout], { type: 'application/octet-stream' }), `${dirname}.${suffix}`)
+        } catch (e) { if (typeof console !== 'undefined') console.log(e, wbout) }
+        return wbout
+      },
+      exportPdf () {
+        this.openMsgBox(dirname => {
+          this._exportPdf(dirname)
+        })
+      },
+      _exportPdf(dirname) {
+        html2Canvas(this.$refs.table.$el, {
+          allowTaint: true
+        }).then(function (canvas) {
+            let contentWidth = canvas.width
+            let contentHeight = canvas.height
+            let pageHeight = contentWidth / 586 * 841.89
+            let leftHeight = contentHeight
+            let position = 0
+            let imgWidth = 595.28
+            let imgHeight = 586 / contentWidth * contentHeight
+            let pageData = canvas.toDataURL('image/jpeg', 1.0)
+            let PDF = new JsPDF('', 'pt', 'a4')
+            if (leftHeight < pageHeight) {
+              PDF.addImage(pageData, 'JPEG', 0, 0, imgWidth, imgHeight)
+            } else {
+              while (leftHeight > 0) {
+                PDF.addImage(pageData, 'JPEG', 0, position, imgWidth, imgHeight)
+                leftHeight -= pageHeight
+                position -= 841.89
+                if (leftHeight > 0) {
+                  PDF.addPage()
+                }
+              }
+            }
+            PDF.save(dirname + '.pdf')
+          }
+        )
       }
     },
     mounted () {
-      this.columnDrop()
-      this.rowDrop()
+      this._columnDrop()
+      this._rowDrop()
     }
   }
 </script>
--- src/config/index.js
+++ src/config/index.js
@@ -6,7 +6,7 @@ export const baseUrlObj = {
 }
 
 // 是否开启线上模式
-export const isOnline = true
+export const isOnline = false
 
 // 导航栏, ''(空)/matched(面包屑导航)/tabs(标签页导航), 标签页会使用页面缓存
 export const navigate = 'tabs'
